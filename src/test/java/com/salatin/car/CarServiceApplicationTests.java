73acf9ea616a71b6edb53a294bc6a6e3af8747b7
Serhii Salatin <lifenjoy8888@gmail.com>
2023-05-02 16:02:33 +0300

    initial commit

---
+++ src/test/java/com/salatin/car/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salatin.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
