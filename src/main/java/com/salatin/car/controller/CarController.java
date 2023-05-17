8794e6742ae36090a760c050a596ca92cfce9fc4
Serhii Salatin <lifenjoy8888@gmail.com>
2023-05-17 14:22:34 +0300

    CarController was created with method create

---
+++ src/main/java/com/salatin/car/controller/CarController.java
@@ -0,0 +1,30 @@
+package com.salatin.car.controller;
+
+import com.salatin.car.model.Car;
+import com.salatin.car.model.dto.request.CarRegistrationRequestDto;
+import com.salatin.car.service.CarService;
+import com.salatin.car.service.mapper.CarMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/cars")
+@RequiredArgsConstructor
+public class CarController {
+    private final CarService carService;
+    private final CarMapper carMapper;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<Car> create(@RequestBody CarRegistrationRequestDto requestDto) {
+        var car = carMapper.toModel(requestDto);
+
+        return carService.save(car);
+    }
+}
